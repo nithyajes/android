e2ae19961a8c45655e6293259ad21346170bfd27
nithyajes <43847191+nithyajes@users.noreply.github.com>
2018-10-04 16:32:04 +0530

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,48 @@
+package com.example.student.myapplication;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+    CheckBox pizza,coffee,burger;
+    Button buttonOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        pizza=(CheckBox)findViewById(R.id.checkBox);
+        coffee=(CheckBox)findViewById(R.id.checkBox2);
+        burger=(CheckBox)findViewById(R.id.checkBox3);
+        buttonOrder=(Button)findViewById(R.id.button);
+
+        //Applying the Listener on the Button click
+        buttonOrder.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int totalamount=0;
+                StringBuilder result=new StringBuilder();
+                result.append("Selected Items:");
+                if(pizza.isChecked()){
+                    result.append("\nPizza 100Rs");
+                    totalamount+=100;
+                }
+                if(coffee.isChecked()){
+                    result.append("\nCoffe 50Rs");
+                    totalamount+=50;
+                }
+                if(burger.isChecked()){
+                    result.append("\nBurger 120Rs");
+                    totalamount+=120;
+                }
+                result.append("\nTotal: "+totalamount+"Rs");
+                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        });
+    }
+}
+
